9c8485c0ba0ac713a3a593789b79b20dbc8453a5
Josephkdominic <104294617+Josephkdominic@users.noreply.github.com>
2024-09-22 17:08:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -55,7 +55,7 @@ async def fil_mod(client, message):
       else:
           await m.edit("Usᴇ :- /autofilter on Oʀ /autofilter off")
 
-@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
